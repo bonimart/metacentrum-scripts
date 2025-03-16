5aedf7a0a0bfd826b8074d7d30cb79e745076008
Martin Beneš <martinbenes626@gmail.com>
2025-03-16 18:29:16 +0100

    add requirements to fetch script

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -5,13 +5,22 @@
 # Then tracks local files and updates them on the remote
 # Does not track file changes on the remote, restart is needed
 
-# Add this to your ~/.ssh/config
+# REQUIREMENTS (otherwise rsync will fail upon asking for password):
+#
+# 1. Generate ssh key
+#
+# 2. Add this to your ~/.ssh/config
+#
 # Host metacentrum
 #   HostName tarkil.grid.cesnet.cz
 #   IdentityFile ~/.ssh/KEYNAME
 #   User USERNAME
 #   PubKeyAuthentication yes
 #   AddKeysToAgent yes
+#
+# 3. Copy public key to metacentrum
+#
+# ssh-copy-id -i ~/.ssh/KEYNAME.pub USERNAME@tarkil.grid.cesnet.cz
 
 DIRS="./labs"
 
